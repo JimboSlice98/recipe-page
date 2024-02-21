cb347d08f709b8d003e89880fb9975b6cd4f0697
Ubuntu <azureuser@virtual-machine.nddealqjdnueddd1qbxx0im1ue.zx.internal.cloudapp.net>
2024-02-21 15:04:59 +0000

    Adding the other microservices: comments

--- comments/api/app.py
+++ comments/api/app.py
@@ -1,65 +1,65 @@
-import json
-import os
-import secrets
-from datetime import timedelta
-
-import requests
+import pyodbc
+from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
-from flask import (Flask, abort, redirect, render_template, request, session,
-                   url_for)
-from flask_login import (LoginManager, current_user, login_required,
-                         login_user, logout_user)
-from oauthlib.oauth2 import WebApplicationClient
-from requests.exceptions import HTTPError, RequestException
+import os, requests
 
-# Configure app.py
+
+load_dotenv()
+
+
 app = Flask(__name__)
 
-# Load environment variables
-# load_dotenv()
 
-# # Get app's secret key so Flask_login can manipulate the session
-# app.config['SECRET_KEY'] = os.environ.get("SECRETKEY")
+@app.route("/get-user-details", methods=["GET"])
+def get_user_details():
+    user_id = request.args.get("user_id")
 
-# # Configure Flask login
-# login_manager = LoginManager(app)
-# login_manager.init_app(app)
-# login_manager.login_view = "login"
+    print(
+        f"Driver   = {{{os.environ['USER_DETAILS_DRIVER']}}};\n"
+        f"Server   = {os.environ['USER_DETAILS_SERVER']};\n"
+        f"Database = {os.environ['USER_DETAILS_DATABASE']};\n"
+        f"UID      = {os.environ['USER_DETAILS_USERNAME']};\n"
+        f"PWD      = {os.environ['USER_DETAILS_PASSWORD']};\n"
+        )
 
-# # Set cookie expiration
-# app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=31)
+    try:
+        conn_str = (
+            f"Driver={{{os.environ['USER_DETAILS_DRIVER']}}};"
+            f"Server={os.environ['USER_DETAILS_SERVER']};"
+            f"Database={os.environ['USER_DETAILS_DATABASE']};"
+            f"UID={os.environ['USER_DETAILS_USERNAME']};"
+            f"PWD={os.environ['USER_DETAILS_PASSWORD']};"
+        )
+        conn = pyodbc.connect(conn_str)
+        cursor = conn.cursor()
+        
+        if user_id:
+            query = "SELECT * FROM user_details WHERE user_id = ?"
+            params = (user_id,)
+        else:
+            query = "SELECT * FROM user_details"
+            params = ()
 
-# # Configure Google OAuth
-# # to work on MacOS, turn off AirPlay receiver and do $ flask run --host=0.0.0.0
-# # will only work on localhost
-# GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-# GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-# GOOGLE_DISCOVERY_URL = (
-#     "https://accounts.google.com/.well-known/openid-configuration")
+        cursor.execute(query, params)
+        rows = cursor.fetchall()
+        
+        users_details = [{"user_id": row.user_id, "cooking_level": row.cooking_level, "birthday": row.birthday.strftime("%Y-%m-%d")} for row in rows]
+        
+        if users_details:
+            return jsonify(users_details)
+        else:
+            return jsonify({"error": "No data found"}), 404
+
+    except pyodbc.Error as e:
+        return jsonify({"error": "Database error", "details": str(e)}), 500
+    except Exception as e:
+        return jsonify({"error": "An unexpected error occurred", "details": str(e)}), 500
 
 
-# # Set up OAuth 2 client
-# client = WebApplicationClient(GOOGLE_CLIENT_ID)
-
-
-# # Define user_loader callback to load user obj from user id in session
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User(id=user_id, username=get_username(user_id))
-
-
-# # Retrieve Google's provider config
-# # ADD ERROR HADNLING TO API CALL LATER
-# def get_google_provider_config():
-#     return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-
-# Configure routing
 @app.route("/", methods=["GET"])
-def index():
+def root():
     return render_template("index.html")
 
 
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html"), 404
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
