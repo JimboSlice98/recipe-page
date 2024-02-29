4890d101745790350378fa259fcb74d567190e91
thomas home <thomaspritch@hotmail.com>
2024-02-29 17:16:16 +0000

    a

--- user-details/api/app.py
+++ user-details/api/app.py
@@ -34,17 +34,31 @@ def get_user_details():
         cursor = conn.cursor()
         
         if user_id:
-            query = "SELECT * FROM user_details WHERE user_id = ?"
+            # query = "SELECT * FROM user_details WHERE user_id = ?"
+            query = "SELECT * FROM user_details_complete WHERE user_id = ?"
             params = (user_id,)
         else:
-            query = "SELECT * FROM user_details"
+            query = "SELECT * FROM user_details_complete"
             params = ()
 
         cursor.execute(query, params)
         rows = cursor.fetchall()
         
-        users_details = [{"user_id": row.user_id, "cooking_level": row.cooking_level, "birthday": row.birthday.strftime("%Y-%m-%d")} for row in rows]
-        
+        # users_details = [{"user_id": row.user_id, "cooking_level": row.cooking_level, "birthday": row.birthday.strftime("%Y-%m-%d")} for row in rows]
+        users_details = [
+            {
+                "user_id": row.user_id,
+                "email": row.email,
+                "display_name": row.display_name,
+                "cooking_level": row.cooking_level,
+                "favorite_cuisine": row.favorite_cuisine,
+                "short_bio": row.short_bio,
+                "profile_picture_url": row.profile_picture_url,
+                "personal_website": row.personal_website,
+                "location": row.location
+            } for row in rows
+        ]
+
         if users_details:
             return jsonify(users_details)
         else:
