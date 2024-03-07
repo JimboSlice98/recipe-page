35beaa37ba624520da9eee70c63a00eb5e268092
Jamie Helsby <jamiehelsby@gmail.com>
2024-03-07 20:27:38 +0000

    cleaning repo

--- comments/create_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Comments (
-    time_stamp VARCHAR(255) PRIMARY KEY,
-    blog_id INT,
-    user_id INT,
-    message VARCHAR(MAX)
-);
--- recipes/create_tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Recipes (
-  recipe_id INT IDENTITY(1,1) PRIMARY KEY,
-  title VARCHAR(255),
-  user_id INT,
-  ingredients VARCHAR(MAX),
-  steps VARCHAR(MAX)
-);
